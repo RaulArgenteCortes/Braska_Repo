e4d2145e43bf9e3c0799cecc1bd719a5f32c9206
antonimf17 <antonimoll.20novembre@gmail.com>
2025-10-28 15:39:25 +0100

    Menu

--- Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
@@ -4,13 +4,23 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string sceneToLoad;
+    [SerializeField] string sceneToLoad1;
+    [SerializeField] string sceneToLoad2;
     public void Play()
     {
-        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad1);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(sceneToLoad2);
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }

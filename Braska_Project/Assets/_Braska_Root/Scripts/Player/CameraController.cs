0ebcbb83f96bb25ae2f4658df556091a2391ca52
RaulArgenteCortes <raulargentecortes@gmail.com>
2025-10-30 09:20:33 +0100

    Ajustes en la camara

--- Braska_Project/Assets/_Braska_Root/Scripts/Player/CameraController.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/Player/CameraController.cs
@@ -1,13 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [Header("Camera stats")]
-    public float startingRotation; // Rotación inicial
-    public float cameraRotation; // Rotación actual
-    public float rotationSpeed; // Velocidad de rotación
-    public float rotationDirection; // Dirección hacia donde rota la cámara
+    public float startingRotation;
+    public float cameraRotation; // Current rotation of the camera
+    public float rotationAcceleration;
+    public float rotationMaxSpeed;
+    public float rotationSpeed;
+    public float rotationInput;
+    public float rotationDirection;
 
     void Start()
     {
@@ -31,15 +35,52 @@ public class CameraController : MonoBehaviour
 
     void RotateCamera()
     {
+        if (rotationInput != 0)
+        {
+            if (rotationSpeed < rotationMaxSpeed && rotationSpeed > -rotationMaxSpeed)
+            {
+                rotationSpeed += rotationAcceleration * rotationDirection;
+            }
+        }
+        else
+        {
+            if (rotationSpeed > rotationAcceleration)
+            {
+                rotationSpeed += rotationAcceleration * -rotationDirection;
+            }
+            else if (rotationSpeed < -rotationAcceleration)
+            {
+                rotationSpeed += rotationAcceleration * -rotationDirection;
+            }
+            else
+            {
+                rotationSpeed = 0;
+            }
+        }
+
+        if (rotationSpeed > rotationMaxSpeed && rotationSpeed < -rotationMaxSpeed)
+        {
+            rotationSpeed = rotationMaxSpeed * rotationDirection;
+        }
+
         // Modifica cameraRotation
-        cameraRotation += rotationSpeed * rotationDirection;
+        cameraRotation += rotationSpeed;
     }
 
     #region Input Methods
 
     public void OnCameraRotate(InputAction.CallbackContext context)
     {
-        rotationDirection = context.ReadValue<float>();
+        rotationInput = context.ReadValue<float>();
+
+        if (rotationInput > 0)
+        {
+            rotationDirection = 1;
+        }
+        else if (rotationInput < 0)
+        {
+            rotationDirection = -1;
+        }
     }
 
     #endregion

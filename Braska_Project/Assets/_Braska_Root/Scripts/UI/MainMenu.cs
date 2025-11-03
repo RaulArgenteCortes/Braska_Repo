605d4b0bf4ac0d0d6692d9f63fbfcd132454e271
antonimf17 <antonimoll.20novembre@gmail.com>
2025-11-03 12:52:54 +0100

    Audio
    Audio

--- Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
@@ -8,19 +8,28 @@ public class MainMenu : MonoBehaviour
     [SerializeField] string sceneToLoad2;
     public void Play()
     {
+        AudioManager.Instance.PlaySFX(0);
         SceneManager.LoadScene(sceneToLoad1);
     }
 
     public void Quit()
     {
+        AudioManager.Instance.PlaySFX(0);
         Application.Quit();
     }
     public void Options()
     {
+        AudioManager.Instance.PlaySFX(0);
         SceneManager.LoadScene(sceneToLoad2);
     }
     public void Menu()
     {
+        AudioManager.Instance.PlaySFX(0);
         SceneManager.LoadScene(sceneToLoad);
     }
+    public void Start()
+    {
+        AudioManager.Instance.PlayMusic(0);
+    }
+
 }

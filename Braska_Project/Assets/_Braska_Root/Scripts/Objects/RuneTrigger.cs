9a503f629d4edb1a409acc5eb64989ceac37f922
antonimf17 <antonimoll.20novembre@gmail.com>
2025-11-25 10:46:32 +0100

    Iluminación
    ilu

--- Braska_Project/Assets/_Braska_Root/Scripts/Objects/RuneTrigger.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/Objects/RuneTrigger.cs
@@ -5,6 +5,22 @@ public class RuneTrigger : MonoBehaviour
     public GameObject vfx_runaActiva;
     public float vfxDuration = 2f;
 
+    public Renderer runeRenderer;           
+    public Color glowColor = Color.cyan;
+
+    public float glowDuration = 20f;
+
+    private Material runeMaterial;
+
+    private void Start()
+    {
+        if (runeRenderer != null)
+        {
+            runeMaterial = runeRenderer.material;
+            runeMaterial.DisableKeyword("_EMISSION");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bark") && ObjectManager.instance.runeCanTrigger)
@@ -13,6 +29,31 @@ public class RuneTrigger : MonoBehaviour
             ObjectManager.instance.RunePrepareMove();
             GameObject vfx = Instantiate(vfx_runaActiva, transform.position, transform.rotation);
             Destroy(vfx, vfxDuration);
+
+          
+
+
+            ActivarIluminacion();
+
+            Invoke(nameof(DesactivarIluminacion), glowDuration);
         }
     }
+    
+    private void ActivarIluminacion()
+    {
+        if (runeMaterial != null)
+        {
+            runeMaterial.EnableKeyword("_EMISSION");
+            runeMaterial.SetColor("_EmissionColor", glowColor * 15f);  // Intensidad del brillo
+        }
+    }
+    private void DesactivarIluminacion()
+    {
+        if (runeMaterial != null)
+        {
+            runeMaterial.SetColor("_EmissionColor", Color.black);
+            runeMaterial.DisableKeyword("_EMISSION");
+        }
+    }
+
 }

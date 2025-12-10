66086cc45ed98d7ca0af69a783fa3057f8c39a71
RaulArgenteCortes <raulargentecortes@gmail.com>
2025-12-10 09:30:43 +0100

    Raíces funcionales

--- Braska_Project/Assets/_Braska_Root/Scripts/Objects/KeyWall.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/Objects/KeyWall.cs
@@ -4,27 +4,43 @@ using UnityEngine.SocialPlatforms.GameCenter;
 public class KeyWall : MonoBehaviour
 {
     [Header("Wall Stats")]
+    [SerializeField] bool playerOnTop;
     [SerializeField] float currentWallPosition;
     private float velocity;
+
+    [Header("Layer Stats")]
+    [SerializeField] GameObject playerCheck;
+    [SerializeField] float playerCheckRadius;
+    [SerializeField] LayerMask playerLayer;
 
     [Header("Object References")]
     [SerializeField] CapsuleCollider wallCollider;
     [SerializeField] GameObject wallMesh;
+
+    private void Update()
+    {
+        CheckUpdate();
+    }
 
     private void FixedUpdate()
     {
         MoveWall();
     }
 
+    private void CheckUpdate()
+    {
+        playerOnTop = Physics.CheckSphere(playerCheck.transform.position, playerCheckRadius, playerLayer);
+    }
+
     private void MoveWall()
     {
         currentWallPosition = Mathf.SmoothDamp(
             currentWallPosition,
-            ObjectManager.instance.openedWallPosition * (ObjectManager.instance.keyHold ? 1 : 0),
+            ObjectManager.instance.openedWallPosition * (ObjectManager.instance.keyHold || playerOnTop ? 1 : 0),
             ref velocity,
             10 / ObjectManager.instance.wallSpeed * Time.deltaTime
         );
-
+        
         wallCollider.center = new Vector3(0, currentWallPosition, 0);
         wallMesh.transform.localPosition = new Vector3(0, currentWallPosition + 0.5f, 0);
     }

a43ebf57c74e6aaa7c8727b316842eb845c1c6e1
RaulArgenteCortes <raulargentecortes@gmail.com>
2025-12-11 13:13:52 +0100

    Flotador + nueva roca grande

---
+++ Braska_Project/Assets/_Braska_Root/Scripts/Interactables/Flotator.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Flotator : MonoBehaviour
+{
+    [Header("Flotatoe stats")]
+    [SerializeField] float startingPoint;
+    [SerializeField] float currentPosition;
+    [SerializeField] float highFloat;
+    [SerializeField] float lowFloat;
+    [SerializeField] float floatSpeed;
+    [SerializeField] bool isHigh;
+    private float velocity;
+
+    private void Start()
+    {
+        startingPoint = transform.position.y;
+        currentPosition = transform.position.y;
+    }
+
+    private void Update()
+    {
+        TrackPosition();
+    }
+
+    private void FixedUpdate()
+    {
+        Flotate();
+    }
+
+    private void TrackPosition()
+    {
+        if (transform.position.y >= startingPoint + highFloat - 0.01f)
+        {
+            isHigh = true;
+        }
+        else if (transform.position.y <= startingPoint + lowFloat + 0.01f)
+        {
+            isHigh = false;
+        }
+    }
+
+    private void Flotate()
+    {
+        currentPosition = Mathf.SmoothDamp(
+            currentPosition,
+            startingPoint + (isHigh ? lowFloat : highFloat),
+            ref velocity,
+            100 / floatSpeed * Time.deltaTime
+        );
+
+        transform.position = new Vector3(transform.position.x, currentPosition, transform.position.z);
+    }
+}

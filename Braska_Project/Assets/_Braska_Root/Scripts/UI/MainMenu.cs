a73209270855248db40874d2948869e1ae80a12c
antonimf17 <antonimoll.20novembre@gmail.com>
2025-12-17 11:37:51 +0100

    RepartButtons
    RepairButtons

--- Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/UI/MainMenu.cs
@@ -7,28 +7,43 @@ public class MainMenu : MonoBehaviour
     [SerializeField] string sceneToLoad;
     [SerializeField] string sceneToLoad1;
     [SerializeField] string sceneToLoad2;
+    public bool isLoading = false;
     #endregion
     #region "Voids"
     public void Play()
     {
+        if (isLoading) return;
+        isLoading = true;
+
         AudioManager.Instance.PlaySFX(0);
         ScenesFade.Instance.FadeOutAndLoad(sceneToLoad1);
         Time.timeScale = 1f;
         ScenesManager.instance.spawnPoint = new Vector3 (-3, 1, 0);
+    
     }
 
     public void Quit()
     {
+        if (isLoading) return;
+        isLoading = true;
+
         AudioManager.Instance.PlaySFX(0);
         Application.Quit();
+      
     }
     public void Options()
     {
+        if (isLoading) return;
+        isLoading = true;
+
         AudioManager.Instance.PlaySFX(0);
         ScenesFade.Instance.FadeOutAndLoad(sceneToLoad2);
     }
     public void Menu()
     {
+        if (isLoading) return;
+        isLoading = true;
+
         AudioManager.Instance.PlaySFX(0);
         ScenesFade.Instance.FadeOutAndLoad(sceneToLoad);
     }
@@ -39,6 +54,9 @@ public class MainMenu : MonoBehaviour
 
     public void Replay()
     {
+        if (isLoading) return;
+        isLoading = true;
+
         ScenesFade.Instance.FadeOutAndLoad(sceneToLoad);
     }
     #endregion

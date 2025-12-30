5d96a8e703bcd60fcf4e906f87627758009f0ee4
RaulArgenteCortes <raulargentecortes@gmail.com>
2025-12-30 16:34:13 +0100

    Mejoras en los covers

--- Braska_Project/Assets/_Braska_Root/Scripts/Misc/CleanCover.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/Misc/CleanCover.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class CleanCover : MonoBehaviour
 {
     [Header("Layer Stats")]
-    [SerializeField] GameObject layerCheck;
+    [SerializeField] GameObject layerCheckOne;
+    [SerializeField] GameObject layerCheckTwo;
     [SerializeField] float layerRadius;
-    [SerializeField] LayerMask layerGround;
+    [SerializeField] LayerMask layerToCheck;
     private bool overlap;
 
     private void Start()
     {
-        overlap = Physics.CheckSphere(layerCheck.transform.position, layerRadius, layerGround);
+        overlap = 
+            Physics.CheckSphere(layerCheckOne.transform.position, layerRadius, layerToCheck)
+            && Physics.CheckSphere(layerCheckTwo.transform.position, layerRadius, layerToCheck);
 
         if (overlap)
         {

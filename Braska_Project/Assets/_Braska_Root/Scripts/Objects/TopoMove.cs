5765c8cbf6a83d86a0e76d71054f7ed45b448df5
RaulArgenteCortes <raulargentecortes@gmail.com>
2026-01-30 10:27:26 +0100

    Nivel 2 arreglado y particulas modificadas

--- Braska_Project/Assets/_Braska_Root/Scripts/Objects/TopoMove.cs
+++ Braska_Project/Assets/_Braska_Root/Scripts/Objects/TopoMove.cs
@@ -15,6 +15,7 @@ public class TopoMove : MonoBehaviour
     [HideInInspector] public bool playerOnTop = false;
     [HideInInspector] public bool playerOnRange = false;
 
+    public ParticleSystem dirtParticles;
 
     public Collider childCollider;
 
@@ -42,6 +43,7 @@ public class TopoMove : MonoBehaviour
         if (other.CompareTag("Bark") && ObjectManager.instance.barkAvailable)
         {
             if (goingUp) return;
+
             Burrow();
         }
     
@@ -85,6 +87,9 @@ public class TopoMove : MonoBehaviour
             animator.SetBool("Unburrow", false);
             animator.SetBool("Idle", false);
         }
+
+        dirtParticles.Play();
+
         CancelInvoke(nameof(TryUnburrow));
         Invoke(nameof(TryUnburrow), timeUnderground);
 
@@ -117,6 +122,8 @@ public class TopoMove : MonoBehaviour
             animator.SetBool("Unburrow", true);
             animator.SetBool("Idle", false);
         }
+
+        dirtParticles.Play();
 
         float unburrowDuration = animator.GetCurrentAnimatorStateInfo(0).length;
         Invoke(nameof(FinishUnburrow), unburrowDuration);
